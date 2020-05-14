07261a0d5f0d28e1d33c94d050030e9806bf202d
Elco Garcia <garciaelco18@gmail.com>
2020-05-14 16:49:35 -0500

    solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,37 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, steps = '') {
+	if (n === row) {
+		return;
+	}
+	//figures out max columns using formula
+	if (steps.length === 2 * n - 1) {
+		console.log(steps);
+		return pyramid(n, row + 1);
+	}
+
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	let add;
+	if (midpoint - row <= steps.length && midpoint + row >= steps.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
+	pyramid(n, row, steps + add);
+}
 
 module.exports = pyramid;
+
+// const midpoint = Math.floor((2 * n - 1) / 2);
+// 	for (let row = 0; row < n; row++) {
+// 		let steps = '';
+// 		for (let column = 0; column < 2 * n - 1; column++) {
+// 			if (midpoint - row <= column && midpoint + row >= column) {
+// 				steps += '#';
+// 			} else {
+// 				steps += ' ';
+// 			}
+// 		}
+// 		console.log(steps);
+// 	}
