2dec4b895c40592146b0a3d99da2430baa46c76d
Elco Garcia <garciaelco18@gmail.com>
2020-05-21 13:40:42 -0500

    solution for fibonacci sequence using cache

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,41 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+	// memoization
+	//store the arguments of each function is called again with the same arguments,
+	// return the precomputed result, than running the function again
+	const cache = {};
+
+	return function(...args) {
+		if (cache[args]) {
+			return cache[args];
+		}
+		const result = fn.apply(this, args);
+		cache[args] = result;
+		return result;
+	};
+}
+
+//exponential time solution = no no no
+//recursion solution
+function fib(n) {
+	if (n < 2) {
+		return n;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+fib = memoize(fib);
 
 module.exports = fib;
+
+//iterative solutions
+//linear runtime
+// let sum = [ 0, 1 ];
+// 	for (i = 2; i <= n; i++) {
+// 		const a = sum[i - 1];
+// 		const b = sum[i - 2];
+// 		sum.push(a + b);
+// 	}
+
+// 	return sum[n];
